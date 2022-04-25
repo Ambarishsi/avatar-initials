ab21958f149b847c926b04719418bf8e24354b69
Ambarishsi <Viratsinghxyz444@gmail.com>
2022-04-26 02:53:06 +0530

    initial checkin

---
+++ README.md
@@ -0,0 +1,35 @@
+# what is this
+
+java script library to generate avatar with name initials:)
+
+# Installation
+
+`npm i avatarjs --save`
+
+usage...
+
+```
+import {avatar_initials, getInitials} from 'avatarjs';
+
+
+avatar_initials({
+    width: '50px',
+    height: '50px',
+    font_weight: '600',
+    background: 'grey',
+    color: 'red',
+    padding: '2px'
+})
+
+```
+
+## Note
+
+need to follow:
+
+* apply class as 'avatar_initials' in your template file.
+* pass the firstName and lastName in the 'getInitials' function.
+    example: 
+        ```<div class="avatar_initials"> {{getInitials('john','doe')}}</div>```
+
+
---
+++ index.js
@@ -0,0 +1,58 @@
+function avatar_initials(options){
+
+    let avatars = document.querySelectorAll('.avatar_initials');
+   
+    avatars.forEach(avatar => {
+
+        avatar.style.display = 'flex';
+        avatar.style.justifyContent = 'center';
+        avatar.style.alignItems = 'center';
+        avatar.style.borderRadius = '100%';
+
+        if(options.width){
+            avatar.style.width = `${options.width}`;
+        }else{
+            avatar.style.width = '50px';
+        }
+
+        if(options.height){
+            avatar.style.height = `${options.height}`;
+        }else{
+            avatar.style.height = '50px';
+        }
+
+        if(options.font_weight){
+            avatar.style.fontWeight = `${options.font_weight}`;
+        }else{
+            avatar.style.fontWeight = '600';
+        }
+
+        if(options.background){
+            avatar.style.background = `${options.background}`;
+        }else{
+            avatar.style.background = 'grey';
+        }
+
+        if(options.color){
+            avatar.style.color = `${options.color}`;
+        }else{
+            avatar.style.color = '#fff';
+        }
+
+        if(options.padding){
+            avatar.style.padding = `${options.padding}`;
+        }else{
+            avatar.style.padding = '2px';
+        }
+
+    });
+}
+
+function getInitials(firstName, lastName) {
+    return firstName[0].toUpperCase() + lastName[0].toUpperCase();
+  }
+
+module.exports = {
+    avatar_initials,
+    getInitials,
+  }
